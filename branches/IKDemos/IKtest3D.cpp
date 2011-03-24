6da6b1988a99ea0d6f34fb4b248122609b19531a
MrYeti1@googlemail.com <MrYeti1@googlemail.com@0e8bac56-0901-9d1a-f0c4-3841fc69e132>
2011-03-24 13:07:41 +0000

    Unworking commit,
    Meredith's changes to IKtest
    CalcEndPos still not working

--- branches/IKDemos/IKtest3D.cpp
+++ branches/IKDemos/IKtest3D.cpp
@@ -1,7 +1,7 @@
 //COMPILE WITH: mkoctfile -lGL -lglut --link-stand-alone IKtest.cpp -o IKtest && ./IKtest
 #include <GL/glut.h>
 
-//#include <octave/oct.h>
+#include <octave/oct.h>
 
 #include <iostream>
 #include <cmath>
@@ -39,6 +39,8 @@ struct NODE
 
 int noofnodes = 0;
 NODE *nodeList[10];
+int nooftests = 0;
+NODE *testList[10];
 
 float IKPosX = 3;
 float IKPosY = 3;
@@ -58,7 +60,7 @@ void light (void)
 	glLightfv(GL_LIGHT0, GL_SPECULAR, whiteSpecularLight);
 	glLightfv(GL_LIGHT0, GL_AMBIENT, blackAmbientLight);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, whiteDiffuseLight);
-	GLfloat LightPosition[] = {2.0f, 2.0f, 5.0f, 0.0f};
+	GLfloat LightPosition[] = {-2.0f, 2.0f, 10.0f, 0.0f};
 	glLightfv(GL_LIGHT0, GL_POSITION, LightPosition);
 	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, whiteSpecularMaterial);
 	glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mShininess);
@@ -79,7 +81,7 @@ void keyPressed (unsigned char key, int x, int y) {
 	keyStates[key] = true;
 	switch (key)
 	{
-		case 'Q':
+		case 'Q': case 0x1b: //0x1B = esc
 			std::cout << "Quitting" << std::endl;
 			exit(0);
 			break;
@@ -112,11 +114,11 @@ void keyPressed (unsigned char key, int x, int y) {
 void keyUp (unsigned char key, int x, int y) { keyStates[key] = false; }
 void keySpecialPressed (int key, int x, int y) { keySpecialStates[key] = true; 
 
-	float d = 0.1f;
-	if (keySpecialStates[GLUT_KEY_LEFT])  { IKPosX -= d; }
-	if (keySpecialStates[GLUT_KEY_RIGHT]) { IKPosX += d; }
-	if (keySpecialStates[GLUT_KEY_DOWN])  { IKPosY -= d; }
-	if (keySpecialStates[GLUT_KEY_UP])    { IKPosY += d; }
+	float d = 0.3f;
+	if (keySpecialStates[GLUT_KEY_LEFT])  { eyeX -= d; }
+	if (keySpecialStates[GLUT_KEY_RIGHT]) { eyeX += d; }
+	if (keySpecialStates[GLUT_KEY_DOWN])  { eyeZ += d; }
+	if (keySpecialStates[GLUT_KEY_UP])    { eyeZ -= d; }
 }
 void keySpecialUp (int key, int x, int y) { keySpecialStates[key] = false; }
 
@@ -133,7 +135,9 @@ void evaluateChain(NODE* seg)
 	//std::cout << seg->name << std::endl;
 	glPushMatrix();
 		glColor3f(0.4f, 0, 0);
-		glRotatef(seg->euler[0], 0, 0, 1);
+		glRotatef(seg->euler[2], 0, 0, 1);
+		glRotatef(seg->euler[1], 0, 1, 0);
+		glRotatef(seg->euler[0], 1, 0, 0);
 		glBegin(GL_LINES);
 			glVertex3f(0, 0, 0);
 			glVertex3f(seg->length[0], seg->length[1], seg->length[2]);
@@ -154,7 +158,6 @@ void setupChain(int j)
 	{
 		while (j>noofnodes)
 		{
-			std::cout << "adding node" << std::endl;
 			NODE *n = new NODE();
 			n->name = "addedNode";
 			n->parent = nodeList[noofnodes-1];
@@ -168,7 +171,6 @@ void setupChain(int j)
 	{
 		while ( j< noofnodes)
 		{
-			std::cout << "removing node" << std::endl;
 			nodeList[noofnodes-2]->child = NULL;
 			delete nodeList[noofnodes-1];
 			noofnodes--;
@@ -216,26 +218,99 @@ void setupChain()
 	n3->name = "upper"; n3->child = n4; n3->parent = n2;
 	n4->name = "hand";  n4->child = NULL; n4->parent = n3;
 
-	n1->length[0] = 0; n1->length[1] = 1; n1->length[2] = 0.5f; n1->weight = 0.3f;
-	n2->length[0] = 0; n2->length[1] = 1; n2->length[2] = 0.5f; n2->weight = 1;
-	n3->length[0] = 0; n3->length[1] = 1; n3->length[2] = 0.5f; n3->weight = 1;
-	n4->length[0] = 0; n4->length[1] = 1; n4->length[2] = 0.5f; n4->weight = 1;
+	n1->length[0] = 0; n1->length[1] = 1; n1->length[2] = 0; n1->weight = 0.3;
+	n2->length[0] = 0; n2->length[1] = 1; n2->length[2] = 0; n2->weight = 1;
+	n3->length[0] = 0; n3->length[1] = 1; n3->length[2] = 0; n3->weight = 1;
+	n4->length[0] = 0; n4->length[1] = 1; n4->length[2] = 0; n4->weight = 1;
 	//std::cout << "n1 Setup" << std::endl;
-	//
 	
 	/*
-	n1->euler = 45;
-	n2->euler = 30;
-	n3->euler = -30;
-	n4->euler = 30;
+	n1->euler[0] = 45 ;	n1->euler[1] = 0;	n1->euler[2] = 0;
+	n2->euler[0] = 20 ;	n2->euler[1] = 0;	n2->euler[2] = 0;
+	n3->euler[0] = -90 ;	n3->euler[1] = 0;	n3->euler[2] = 0;
+	n4->euler[0] = 25 ;	n4->euler[1] = 0;	n4->euler[2] = 0;
 	*/
-	
+	n1->euler[0] = 45;	n1->euler[1] = 0;	n1->euler[2] = 45;
+	n2->euler[0] = -90;	n2->euler[1] = 0;	n2->euler[2] = 0;
+	n3->euler[0] = 0;	n3->euler[1] = 0;	n3->euler[2] = -90;
+	n4->euler[0] = 0;	n4->euler[1] = 0;	n4->euler[2] = 25;
 	
 	nodeList[noofnodes++] = n1; //noofnodes++;
 	nodeList[noofnodes++] = n2; //noofnodes++;
 	nodeList[noofnodes++] = n3; //noofnodes++;
 	nodeList[noofnodes++] = n4; //noofnodes++;
 	//std::cout << "LEaving Exit" << std::endl;
+	//
+	NODE *na = new NODE;
+	na->name = "test1";
+	na->length[0] = 0; na->length[1] = 1; na->length[2] = 0; na->weight = 1;
+	na->euler[0] = 0;	na->euler[1] = 0;	na->euler[2] = 0;
+	na->child = NULL; na->parent = NULL;
+	testList[nooftests++] = na;
+
+	NODE *nb = new NODE;
+	nb->name = "test2";
+	nb->length[0] = 0; nb->length[1] = 2; nb->length[2] = 0; nb->weight = 1;
+	nb->euler[0] = 45;	nb->euler[1] = 0;	nb->euler[2] = 0;
+	nb->child = NULL; nb->parent = NULL;
+	testList[nooftests++] = nb;
+
+	NODE *ne = new NODE;
+	ne->name = "test5";
+	ne->length[0] = 0; ne->length[1] = 1; ne->length[2] = 0; ne->weight = 1;
+	ne->euler[0] = 45;	ne->euler[1] = 0;	ne->euler[2] = -80;
+	ne->child = NULL; ne->parent = NULL;
+	testList[nooftests++] = ne;
+
+	NODE *ng = new NODE;
+	ng->name = "test5";
+	ng->length[0] = 0; ng->length[1] = 1; ng->length[2] = 0; ng->weight = 1;
+	ng->euler[0] = 45;	ng->euler[1] = 30;	ng->euler[2] = -80;
+	ng->child = NULL; ng->parent = NULL;
+	testList[nooftests++] = ng;
+
+
+
+	NODE *nc = new NODE;
+	NODE *nd = new NODE;
+	NODE *nf = new NODE;
+	nc->name = "test-3";
+	nc->length[0] = 0; nc->length[1] = 1; nc->length[2] = 0; nc->weight = 1;
+	nc->child = nd; nc->parent = NULL;
+	testList[nooftests++] = nc;
+
+	nd->name = "test-3a";
+	nd->length[0] = 0; nd->length[1] = 1; nd->length[2] = 0; nd->weight = 1;
+	nd->child = nf; nd->parent = nc;
+
+	testList[nooftests++] = nd;
+	nf->name = "test-3b";
+	nf->length[0] = 0; nf->length[1] = 1; nf->length[2] = 0; nf->weight = 1;
+	nf->child = NULL; nf->parent = nd;
+	testList[nooftests++] = nf;
+
+	nc->euler[0] = 30;	nc->euler[1] = 0;	nc->euler[2] = 0;
+	nd->euler[0] = -60;	nd->euler[1] = 0;	nd->euler[2] = 0;
+	nf->euler[0] = 0;	nf->euler[1] = 0;	nf->euler[2] = 60;
+
+
+	NODE *nh = new NODE;
+	NODE *ni = new NODE;
+	nh->name = "test-2";
+	nh->length[0] = 0; nh->length[1] = 1; nh->length[2] = 0; nh->weight = 1;
+	nh->child = ni; nh->parent = NULL;
+	testList[nooftests++] = nh;
+
+	ni->name = "test-2a";
+	ni->length[0] = 0; ni->length[1] = 1; ni->length[2] = 0; ni->weight = 1;
+	ni->child = NULL; ni->parent = nh;
+	testList[nooftests++] = ni;
+
+	nh->euler[0] = 0;	nh->euler[1] = 0;	nh->euler[2] = -40;
+	ni->euler[0] = 0;	ni->euler[1] = 0;	ni->euler[2] = -40;
+
+
+
 
 	normaliseWeights(n1);
 	return;
@@ -253,9 +328,42 @@ NODE* getEndEffector(NODE *seg)
 	}
 }
 
+Matrix fillRotMat(float a, float b, float g)
+{
+	//XYZ Matrix - Wikipedia?
+	Matrix rotMat = Matrix(3,3); rotMat.fill(0);
+	rotMat(0,0) = cos(b)*cos(g);
+	rotMat(0,1) = -cos(a)*sin(g) + sin(a)*sin(b)*cos(g);;
+	rotMat(0,2) = sin(a)*sin(g) + cos(a)*sin(b)*cos(g);
+
+	rotMat(1,0) = cos(b)*cos(g);
+	rotMat(1,1) = cos(a)*cos(g) + sin(a)*sin(b)*sin(g);
+	rotMat(1,2) = -sin(a)*cos(g) + cos(a)*sin(b)*sin(g);
+
+	rotMat(2,0) = -sin(b);
+	rotMat(2,1) = sin(a)*cos(b);
+	rotMat(2,2) = cos(a)*cos(b);
+
+	return rotMat;
+
+	/* // ZYX -- presentation order
+	rotMat(0,0) = cos(a)*cos(b)*cos(g)-sin(a)*sin(g);
+	rotMat(0,1) = -cos(a)*cos(b)*sin(g)-sin(a)*cos(g);
+	rotMat(0,2) = cos(a)*sin(b);
+
+	rotMat(1,0) = sin(a)*cos(b)*cos(g)+cos(a)*sin(g);
+	rotMat(1,1) = -sin(a)*cos(b)*sin(g)+cos(a)*cos(g);
+	rotMat(1,2) = sin(a)*sin(b);
+
+	rotMat(2,0) = -sin(b)*cos(g);
+	rotMat(2,1) = sin(b)*sin(g);
+	rotMat(2,2) = -cos(b);
+	*/
+}
+
 /*
  * calcEndPos
- * take a NODE and an array of length 3
+ * take a NODE and a float array of length 3
  * fill the array with x,y,z coords of the nodes's end position
  */
 void calcEndPos(NODE *end, float *pos)
@@ -271,19 +379,94 @@ void calcEndPos(NODE *end, float *pos)
 		pos[0] = 0; pos[1] = 0; pos[2] = 0;
 	}
 	//Iteratively get the orientation given the parent nodes
-	float pTheta = 0;
-	while (end->parent) { pTheta += end->parent->euler[0]; end = end->parent;	}
+	/*
+	float pThetaX, pThetaY, pThetaZ;
+	pThetaX = 0;
+	pThetaY = 0;
+	pThetaZ = 0;
+	*/
+	Matrix pRotMatF = Matrix(3,3).fill(0);
+	Matrix pRotMat = Matrix(3,3).fill(0);
+	while ((end->parent != NULL)) { 
+		end = end->parent;
+	}
+
+	while (end != thisEnd)
+	{
+
+		Matrix pRotMatT = Matrix(3,3).fill(0);
+		pRotMatT = fillRotMat(degRad(end->euler[0]), degRad(end->euler[1]), degRad(end->euler[2]));
+		if ((mx_el_eq(pRotMat, 0) == boolMatrix(3,3,1))){
+		   	pRotMatF = pRotMatT; 
+		} else {
+			pRotMatF = pRotMat *pRotMatT;
+		}
+		pRotMat = pRotMatF;
+		std::cout << "pRotMat" << pRotMatF << std::endl;
+
+	//	pThetaX += end->parent->euler[0]; 
+	//	pThetaY += end->parent->euler[1]; 
+	//	pThetaZ += end->parent->euler[2]; 
+		end = end->child;	
+	}
 	end = thisEnd;
+	
+
+	//std::cout << pRotMatF << std::endl;
+	/*
+	std::cout << pThetaX << " ";
+	std::cout << pThetaY << " ";
+	std::cout << pThetaZ << " " << std::endl;
+	*/
 
 	//length of node and angle from vertical.
+	float a, b, g;
+	a = degRad(end->euler[0]);
+	b = degRad(end->euler[1]);
+	g = degRad(end->euler[2]);
+
+	Matrix rotMat = Matrix(3,3);
+	Matrix tRotMat = Matrix(3,3);
+	rotMat = fillRotMat(a,b,g);
+	ColumnVector lenVect = ColumnVector(3);
+	ColumnVector posVect = ColumnVector(3);
+
+	lenVect(0) = end->length[0];
+	lenVect(1) = end->length[1];
+	lenVect(2) = end->length[2];
+
+	std::cout << (bool)(mx_el_eq(pRotMat, 0) == boolMatrix(3,3,1)) << std::endl;
+	if (mx_el_eq(pRotMat, 0) == boolMatrix(3,3,1)) {
+		tRotMat = rotMat;
+	} else {
+		tRotMat = rotMat*pRotMat;
+	}
+	std::cout << tRotMat << std::endl;
+
+	posVect = tRotMat*lenVect;
+
+	//std::cout << radDeg(a) << " " << radDeg(b) << " " << radDeg(g)  << std::endl;
+	//std::cout << rotMat << std::endl;
+	//std::cout << posVect << std::endl;
+
+
 	float h = sqrt(end->length[0]*end->length[0] + end->length[1]*end->length[1] + end->length[2]*end->length[2]);
-	float theta = atan2(end->length[0], end->length[1]);
+	float h2 = sqrt(posVect(0)*posVect(0) + posVect(1)*posVect(1) + posVect(2)*posVect(2));
+
+	//assert(h == h2);
+
+	//float theta = atan2(end->length[0], end->length[1]);
 
 	//std::cout << end->name << " h = " << h << ", theta = " << radDeg(theta) << std::endl;
 
+	pos[0] += posVect(0);
+	pos[1] += posVect(1);
+	pos[2] += posVect(2);
+
 	//Simple Trig to get the end vector position of this node.
-	pos[0] += h*-sin(theta + degRad(end->euler[0]+pTheta));
-	pos[1] += h*cos(theta + degRad(end->euler[0]+pTheta));
+	//pos[0] += h*-sin(theta + degRad(end->euler[0]+pThetaX));
+	//pos[1] += h*cos(theta + degRad(end->euler[0]+pThetaX));
+	//pos[2] += 0; 
 
 	//std::cout << end->name << " End Pos = " << pos[0] << ", " << pos[1] << std::endl;
 
@@ -466,12 +649,29 @@ void display(void)
 
 
 	//std::cout << "EVALUATEING CHAIN" << std::endl;
-	evaluateChain(nodeList[0]);
+
+	for (int i=0; i<nooftests; i++)
+	{
+		std::cout << testList[i]->name << std::endl;
+		if (testList[i]->parent == NULL)
+		{
+			evaluateChain(testList[i]);
+		}
+		float pos[3];
+		pos[0] = 0; pos[1] = 0; pos[2] = 0;
+		calcEndPos(testList[i], pos);
+		glPushMatrix();
+		 glTranslatef(pos[0], pos[1], pos[2]);
+		 glutSolidSphere(0.1, 5, 5);
+		glPopMatrix();
+	}
+	
+	//evaluateChain(nodeList[0]);
 
 	if (doJac)
 	{
 		//jacobian(nodeList[0]);
-	} else {
+	} else if (false) {
 		NODE *node = getEndEffector(nodeList[0]);
 		float pos[3];
 		while (node)
@@ -482,7 +682,7 @@ void display(void)
 			 glTranslatef(pos[0], pos[1], pos[2]);
 			 glutSolidSphere(0.1, 5, 5);
 			glPopMatrix();
-			CCD(node);
+			//CCD(node);
 			node = node->parent;
 		}
 
@@ -501,7 +701,7 @@ void display(void)
 
 	glPushMatrix();
 		glColor4f(0.9f, 0.8f, 0.8f, 0.05f);
-		glTranslatef(0, 0, -0.05f);
+		//glTranslatef(0, 0, -0.05f);
 		glScalef(1, 1, 0.001f);
 		glutSolidSphere(noofnodes, 17, 17);
 	glPopMatrix();
