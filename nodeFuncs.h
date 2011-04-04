c254bd731d6d0b8b658f057852bbf1bf30a2fcb4
MrYeti1@googlemail.com <MrYeti1@googlemail.com@0e8bac56-0901-9d1a-f0c4-3841fc69e132>
2011-04-04 21:45:39 +0000

    fillRotMat takes a matrix and no longer uses Octave

--- nodeFuncs.h
+++ nodeFuncs.h
@@ -30,6 +30,23 @@ NODE* getEndEffector(NODE *seg)
 	}
 }
 
+void fillRotMat(float a, float b, float g, float *rotMat)
+{
+	rotMat[(0*3)+0] = cos(b)*cos(g);
+	rotMat[(0*3)+1] = -cos(a)*sin(g) + sin(a)*sin(b)*cos(g);;
+	rotMat[(0*3)+2] = sin(a)*sin(g) + cos(a)*sin(b)*cos(g);
+
+	rotMat[(1*3)+0] = cos(b)*sin(g);
+	rotMat[(1*3)+1] = cos(a)*cos(g) + sin(a)*sin(b)*sin(g);
+	rotMat[(1*3)+2] = -sin(a)*cos(g) + cos(a)*sin(b)*sin(g);
+
+	rotMat[(2*3)+0] = -sin(b);
+	rotMat[(2*3)+1] = sin(a)*cos(b);
+	rotMat[(2*3)+2] = cos(a)*cos(b);
+	
+	return;
+}
+
 Matrix fillRotMat(float a, float b, float g)
 {
 	Matrix rotMat = Matrix(3,3); rotMat.fill(0);
@@ -171,4 +188,47 @@ void calcEndPos(NODE *end, float *pos)
 	//std::cout << end->name << " End Pos = " << pos[0] << ", " << pos[1] << std::endl;
 
 	return;
+}
+
+void radAngleAxisRot(float rotAng, float *axis, float *eulers)
+{
+	Matrix axisMat = Matrix(3,3);
+	axisMat(0,0) = 1+ (1-cos(rotAng))*(axis[0]*axis[0]-1);
+	axisMat(0,1) = -axis[2]*sin(rotAng)+(1-cos(rotAng))*axis[0]*axis[1];
+	axisMat(0,2) = axis[1]*sin(rotAng)+(1-cos(rotAng))*axis[0]*axis[2];
+
+	axisMat(1,0) = axis[2]*sin(rotAng)+(1-cos(rotAng))*axis[0]*axis[1];
+	axisMat(1,1) = 1 + (1-cos(rotAng))*(axis[1]*axis[1]-1);
+	axisMat(1,2) = -axis[0]*sin(rotAng)+(1-cos(rotAng))*axis[1]*axis[2];
+	
+	axisMat(2,0) = -axis[1]*sin(rotAng)+(1-cos(rotAng))*axis[0]*axis[2];
+	axisMat(2,1) = axis[0]*sin(rotAng)+(1-cos(rotAng))*axis[1]*axis[2];
+	axisMat(2,2) = 1 + (1-cos(rotAng))*(axis[2]*axis[2]-1);
+
+	float x, y, z;
+	
+	if (axisMat(1,0) > 0.998)
+	{
+		std::cout << "sing NORTH" << std::endl;
+		 //Singluarity at north Pole
+		 y = atan2(axisMat(0,2), axisMat(2,2));
+		 x = PI/2;
+		 z = 0;
+	} else if (axisMat(1,0) < -0.998)
+	{
+		std::cout << "sing SOUTH" << std::endl;
+		//Singularit at south pole
+		y = atan2(axisMat(0,2), axisMat(2,2));
+		x = -PI/2;
+		z = 0;
+	} else {
+		y = atan2(-axisMat(2,0), axisMat(0,0));
+		x = atan2(-axisMat(1,2), axisMat(1,1));
+		z = asin(-axisMat(1,0));
+	}
+
+	eulers[0] = x;
+	eulers[1] = y;
+	eulers[2] = z;
+
 }
